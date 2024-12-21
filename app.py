8133cf1579234bd4b1b6560a1282392298face5a
Gitb-24 <flairclick61@gmail.com>
2024-12-22 00:13:37 +0530

    Added form input validation

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, flash
+import db
 
 app = Flask(__name__)
-DATABASE = 'database.db'
-
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
+app.config['SECRET_KEY'] = 'your_secret_key' # Required for flash messages
 
 @app.route("/")
 def index():
-    conn = get_db_connection()
-    vehicles = conn.execute('SELECT * FROM Vehicles').fetchall()
-    conn.close()
+    vehicles = db.get_vehicles()
     return render_template('index.html', vehicles=vehicles)
 
 @app.route("/bookings")
 def bookings():
-  conn = get_db_connection()
-  bookings = conn.execute('SELECT * FROM Bookings').fetchall()
-  conn.close()
-  return render_template('bookings.html', bookings=bookings)
+    bookings = db.get_bookings()
+    return render_template('bookings.html', bookings=bookings)
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
@@ -31,14 +22,32 @@ def create():
         customer_name = request.form['customer_name']
         contact_info = request.form['contact_info']
 
-        conn = get_db_connection()
-        conn.execute('INSERT INTO Bookings (VehicleID, BookingDate, CustomerName, ContactInfo) VALUES (?, ?, ?, ?)',
-                     (vehicle_id, booking_date, customer_name, contact_info))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('bookings'))
+        if not vehicle_id:
+            flash('Vehicle ID is required!')
+        elif not booking_date:
+            flash('Booking date is required!')
+        elif not customer_name:
+            flash('Customer name is required!')
+        elif not contact_info:
+            flash('Contact info is required!')
+        else:
+            try:
+                vehicle_id = int(vehicle_id) # Try to convert to integer
+            except ValueError:
+                flash('Invalid Vehicle ID')
+                return render_template('create_booking.html')
 
+            conn = db.get_db_connection()
+            try:
+                conn.execute('INSERT INTO Bookings (VehicleID, BookingDate, CustomerName, ContactInfo) VALUES (?, ?, ?, ?)',
+                         (vehicle_id, booking_date, customer_name, contact_info))
+                conn.commit()
+                conn.close()
+                return redirect(url_for('bookings'))
+            except sqlite3.IntegrityError:
+                flash('Vehicle with this ID does not exist')
+                conn.close()
+                return render_template('create_booking.html')
+
+        return render_template('create_booking.html')
     return render_template('create_booking.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
