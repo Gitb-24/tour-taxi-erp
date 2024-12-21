13627af61317e8ebe3d495156840c18e371b2284
Gitb-24 <flairclick61@gmail.com>
2024-12-21 23:49:37 +0530

    Added bookings page

--- app.py
+++ app.py
@@ -14,7 +14,14 @@ def index():
     conn = get_db_connection()
     vehicles = conn.execute('SELECT * FROM Vehicles').fetchall()
     conn.close()
-    return render_template('index.html', vehicles=vehicles)  # Pass data to template
+    return render_template('index.html', vehicles=vehicles)
+
+@app.route("/bookings")  # New route for bookings
+def bookings():
+    conn = get_db_connection()
+    bookings = conn.execute('SELECT * FROM Bookings').fetchall()
+    conn.close()
+    return render_template('bookings.html', bookings=bookings)
 
 if __name__ == "__main__":
     app.run(debug=True)
